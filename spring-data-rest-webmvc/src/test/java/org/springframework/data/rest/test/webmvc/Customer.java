aff62b7cd750dc46cae5eb8d93535ae7ed764049
Jon Brisbin <jbrisbin@vmware.com>
2012-10-10 08:56:29 -0500

    Added specific message key for testing `@NotNull` handling.

--- spring-data-rest-webmvc/src/test/java/org/springframework/data/rest/test/webmvc/Customer.java
+++ spring-data-rest-webmvc/src/test/java/org/springframework/data/rest/test/webmvc/Customer.java
@@ -12,7 +12,7 @@ import javax.validation.constraints.NotNull;
 public class Customer {
 
   @Id @GeneratedValue private Long   id;
-  @NotNull
+  @NotNull(message = "no.userid")
   private                     String userid;
 
   public Long getId() {
