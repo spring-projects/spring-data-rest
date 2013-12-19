204d2870e758a58a44536c39416e4e680b57245b
Oliver Gierke <ogierke@gopivotal.com>
2013-12-19 14:52:44 +0100

    DATAREST-209 - Removed hard dependency on Jackson Hibernate 4 module.
    The build script now declares the Jackson Hibernate 4 as optional dependency to avoid being registered in a non-Hibernate environment. Tightened auto-registration checks to also check for Hibernate 4 presence at all.
    
    We automatically activate verbose date rendering when registering the Joda Time module.

--- spring-data-rest-webmvc/src/main/java/org/springframework/data/rest/webmvc/json/Jackson2DatatypeHelper.java
+++ spring-data-rest-webmvc/src/main/java/org/springframework/data/rest/webmvc/json/Jackson2DatatypeHelper.java
@@ -1,46 +1,76 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.data.rest.webmvc.json;
 
-import com.fasterxml.jackson.databind.Module;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.hibernate.Version;
 import org.springframework.util.ClassUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
 
 /**
  * Helper class to register datatype modules based on their presence in the classpath.
  * 
  * @author Jon Brisbin
+ * @author Oliver Gierke
  */
 public class Jackson2DatatypeHelper {
 
-	private static final Logger LOG = LoggerFactory.getLogger(Jackson2DatatypeHelper.class);
+	private static final boolean IS_HIBERNATE_AVAILABLE = ClassUtils.isPresent("org.hibernate.Version",
+			Jackson2DatatypeHelper.class.getClassLoader());
 	private static final boolean IS_HIBERNATE4_MODULE_AVAILABLE = ClassUtils.isPresent(
 			"com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module", Jackson2DatatypeHelper.class.getClassLoader());
+
 	private static final boolean IS_JODA_MODULE_AVAILABLE = ClassUtils.isPresent(
 			"com.fasterxml.jackson.datatype.joda.JodaModule", Jackson2DatatypeHelper.class.getClassLoader());
 
 	public static void configureObjectMapper(ObjectMapper mapper) {
+
 		// Hibernate types
-		if (IS_HIBERNATE4_MODULE_AVAILABLE) {
-			try {
-				mapper.registerModule((Module) Class.forName("com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module")
-						.newInstance());
-			} catch (Throwable t) {
-				if (LOG.isDebugEnabled()) {
-					LOG.debug(t.getMessage(), t);
-				}
-			}
+		if (IS_HIBERNATE_AVAILABLE && Hibernate4Checker.isHibernate4() && IS_HIBERNATE4_MODULE_AVAILABLE) {
+			Hibernate4ModuleRegistrar.registerModule(mapper);
 		}
+
 		// JODA time
 		if (IS_JODA_MODULE_AVAILABLE) {
-			try {
-				mapper.registerModule((Module) Class.forName("com.fasterxml.jackson.datatype.joda.JodaModule").newInstance());
-			} catch (Throwable t) {
-				if (LOG.isDebugEnabled()) {
-					LOG.debug(t.getMessage(), t);
-				}
-			}
+			JodaModuleRegistrar.registerModule(mapper);
 		}
 	}
 
+	private static class Hibernate4Checker {
+
+		public static boolean isHibernate4() {
+			return Version.getVersionString().startsWith("4");
+		}
+	}
+
+	private static class Hibernate4ModuleRegistrar {
+
+		public static void registerModule(ObjectMapper mapper) {
+			mapper.registerModule(new Hibernate4Module());
+		}
+	}
+
+	private static class JodaModuleRegistrar {
+
+		public static void registerModule(ObjectMapper mapper) {
+			mapper.registerModule(new JodaModule());
+			mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		}
+	}
 }
