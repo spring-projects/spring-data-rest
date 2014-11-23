a31588cabce8c99e35b7044d57dd3379e333a474
Greg Turnquist <gturnquist@gopivotal.com>
2014-11-22 21:00:09 -0600

    DATAREST-414 - Added integration tests for Cassandra.
    Original pull request: #158.

---
+++ spring-data-rest-webmvc/src/test/java/org/springframework/data/rest/webmvc/cassandra/CassandraProperties.java
@@ -0,0 +1,7 @@
+package org.springframework.data.rest.webmvc.cassandra;
+
+public class CassandraProperties {
+
+	public static final String HOSTNAME = "localhost";
+	public static final int PORT = 9142;
+}
