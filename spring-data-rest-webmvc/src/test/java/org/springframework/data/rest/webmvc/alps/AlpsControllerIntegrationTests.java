28e0ef76d1a6492d31885d9c150f68abbc6eb7c1
Oliver Gierke <ogierke@pivotal.io>
2015-04-13 12:18:24 +0200

    DATAREST-516 - Fixed link to ALPS representation descriptors of associations.
    Previously the link pointing to representation descriptors of associations neither pointed to the ALPS controller nor to the representation descriptor.

--- spring-data-rest-webmvc/src/test/java/org/springframework/data/rest/webmvc/alps/AlpsControllerIntegrationTests.java
+++ spring-data-rest-webmvc/src/test/java/org/springframework/data/rest/webmvc/alps/AlpsControllerIntegrationTests.java
@@ -36,7 +36,6 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -138,8 +137,26 @@ public class AlpsControllerIntegrationTests extends AbstractControllerIntegratio
 
 		assertThat(usersLink, is(notNullValue()));
 
-		mvc.perform(get(usersLink.getHref())).andDo(MockMvcResultHandlers.print())
-				.andExpect(jsonPath("$.descriptors[?(@.id == 'item-representation')].href", is(notNullValue())));
+		mvc.perform(get(usersLink.getHref())).andExpect(
+				jsonPath("$.descriptors[?(@.id == 'item-representation')].href", is(notNullValue())));
+	}
+
+	/**
+	 * @see DATAREST-516
+	 */
+	@Test
+	public void referenceToAssociatedEntityDesciptorPointsToRepresentationDescriptor() throws Exception {
+
+		Link profileLink = discoverUnique("/", "profile");
+		Link usersLink = discoverUnique(profileLink.getHref(), "people");
+
+		String jsonPath = "$."; // Root
+		jsonPath += "descriptors[?(@.id == 'person-representation')]."; // Representation descriptor
+		jsonPath += "descriptors[?(@.name == 'father')][0]."; // First father descriptor
+		jsonPath += "rt"; // Return type
+
+		mvc.perform(get(usersLink.getHref())).andExpect(
+				jsonPath(jsonPath, allOf(containsString("alps"), endsWith("-representation"))));
 	}
 
 	private Link discoverUnique(String href, String rel) throws Exception {
