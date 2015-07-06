be8964c46538a8de802e67ba23f64fbfbd5b4105
Oliver Gierke <ogierke@pivotal.io>
2015-07-06 11:42:35 +0200

    DATAREST-582 - Fixed potential double invocation of annotated event handlers.
    AnnotatedEventHandlerInvoker now uses the user class to detect handler methods on to make sure we don't accidentally find CGLib generated methods and thus invoke the handler multiple times later on.
    
    Extended MethodFilter Methods.USER_METHODS to filter methods on CGLib proxy classes, too.
    
    Added missing license header and Javadocs where necessary.

--- spring-data-rest-core/src/main/java/org/springframework/data/rest/core/util/Methods.java
+++ spring-data-rest-core/src/main/java/org/springframework/data/rest/core/util/Methods.java
@@ -1,10 +1,28 @@
+/*
+ * Copyright 2012-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.data.rest.core.util;
 
 import java.lang.reflect.Method;
 
+import org.springframework.util.ClassUtils;
 import org.springframework.util.ReflectionUtils;
 
 /**
+ * Simple helper with utilities to work with {@link Method}s.
+ * 
  * @author Jon Brisbin
  * @author Oliver Gierke
  */
@@ -20,8 +38,12 @@ public abstract class Methods {
 		 */
 		@Override
 		public boolean matches(Method method) {
-			return (!method.isSynthetic() && !method.isBridge() && method.getDeclaringClass() != Object.class && !method
-					.getName().contains("$"));
+			
+			return !method.isSynthetic() && //
+					!method.isBridge() && //
+					!ReflectionUtils.isObjectMethod(method) && //
+					!ClassUtils.isCglibProxyClass(method.getDeclaringClass()) && //
+					!ReflectionUtils.isCglibRenamedMethod(method);
 		}
 	};
 }
---
+++ spring-data-rest-core/src/test/java/org/springframework/data/rest/core/event/AnnotatedEventHandlerInvokerUnitTests.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.rest.core.event;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.data.rest.core.event.AnnotatedEventHandlerInvoker.EventHandlerMethod;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.util.MultiValueMap;
+
+/**
+ * Unit tests for {@link AnnotatedEventHandlerInvoker}.
+ * 
+ * @author Oliver Gierke
+ */
+public class AnnotatedEventHandlerInvokerUnitTests {
+
+	/**
+	 * @see DATAREST-582
+	 */
+	@Test
+	@SuppressWarnings("unchecked")
+	public void doesNotDiscoverMethodsOnProxyClasses() {
+
+		ProxyFactory factory = new ProxyFactory();
+		factory.setTarget(new Sample());
+		factory.setProxyTargetClass(true);
+
+		AnnotatedEventHandlerInvoker invoker = new AnnotatedEventHandlerInvoker();
+		invoker.postProcessAfterInitialization(factory.getProxy(), "proxy");
+
+		MultiValueMap<Class<? extends RepositoryEvent>, EventHandlerMethod> methods = (MultiValueMap<Class<? extends RepositoryEvent>, EventHandlerMethod>) ReflectionTestUtils
+				.getField(invoker, "handlerMethods");
+
+		assertThat(methods.get(BeforeCreateEvent.class), hasSize(1));
+	}
+
+	@RepositoryEventHandler
+	static class Sample {
+
+		@HandleBeforeCreate
+		public void method(Sample sample) {}
+	}
+}
