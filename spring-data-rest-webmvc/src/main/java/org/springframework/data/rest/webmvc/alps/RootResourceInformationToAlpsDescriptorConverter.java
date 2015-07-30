1f9dc1a6464fc0dc015256908a87c110c073528a
Greg Turnquist <gturnquist@pivotal.io>
2015-07-30 18:21:35 -0500

    DATAREST-630 - ALPS metadata now only exposes identifier property if explicitly configured to.
    The ALPS metadata exposed now considers the id exposure configuration to decide whether to skip the identifier property or not.
    
    Original pull request: #189.

--- spring-data-rest-webmvc/src/main/java/org/springframework/data/rest/webmvc/alps/RootResourceInformationToAlpsDescriptorConverter.java
+++ spring-data-rest-webmvc/src/main/java/org/springframework/data/rest/webmvc/alps/RootResourceInformationToAlpsDescriptorConverter.java
@@ -50,8 +50,11 @@ import org.springframework.data.rest.core.mapping.SimpleResourceDescription;
 import org.springframework.data.rest.core.mapping.SupportedHttpMethods;
 import org.springframework.data.rest.webmvc.RootResourceInformation;
 import org.springframework.data.rest.webmvc.json.JacksonMetadata;
+import org.springframework.data.rest.webmvc.json.JsonSchema;
 import org.springframework.data.rest.webmvc.mapping.AssociationLinks;
 import org.springframework.data.rest.webmvc.mapping.PropertyMappings;
+import org.springframework.data.util.ClassTypeInformation;
+import org.springframework.data.util.TypeInformation;
 import org.springframework.hateoas.EntityLinks;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.TemplateVariable;
@@ -305,7 +308,7 @@ public class RootResourceInformationToAlpsDescriptorConverter {
 		return descriptors;
 	}
 
-	private List<Descriptor> buildPropertyDescriptors(Class<?> type, final String baseRel) {
+	private List<Descriptor> buildPropertyDescriptors(final Class<?> type, String baseRel) {
 
 		final PersistentEntity<?, ?> entity = persistentEntities.getPersistentEntity(type);
 		final List<Descriptor> propertyDescriptors = new ArrayList<Descriptor>();
@@ -322,6 +325,15 @@ public class RootResourceInformationToAlpsDescriptorConverter {
 				ResourceMapping propertyMapping = propertyMappings.getMappingFor(property);
 
 				if (propertyDefinition != null) {
+					PersistentProperty<?> persistentProperty = entity.getPersistentProperty(propertyDefinition.getInternalName());
+					TypeInformation<?> propertyType = persistentProperty == null ? ClassTypeInformation.from(propertyDefinition
+							.getPrimaryMember().getRawType()) : persistentProperty.getTypeInformation();
+					Class<?> rawEntityType = entity.getTypeInformation().getType();
+
+					if (persistentProperty.isIdProperty() && !configuration.isIdExposedFor(type)) {
+						return;
+					}
+
 					propertyDescriptors.add(//
 							descriptor(). //
 									type(Type.SEMANTIC).//
