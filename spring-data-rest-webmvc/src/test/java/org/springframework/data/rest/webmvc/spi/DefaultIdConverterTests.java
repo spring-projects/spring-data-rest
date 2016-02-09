0d81b0fd1d9d6ca6f8a2f2e4f256ac4c4553ae57
Andrew Walters <andrew.mark.walters@gmail.com>
2016-02-09 13:11:53 +0000

    Reformatting

--- spring-data-rest-webmvc/src/test/java/org/springframework/data/rest/webmvc/spi/DefaultIdConverterTests.java
+++ spring-data-rest-webmvc/src/test/java/org/springframework/data/rest/webmvc/spi/DefaultIdConverterTests.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *	  http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -24,74 +24,74 @@ import static org.springframework.data.rest.webmvc.spi.BackendIdConverter.Defaul
 
 /**
  * Unit tests for {@link BackendIdConverter.DefaultIdConverter}.
- *
+ * 
  * @author Andrew Walters
  */
 public class DefaultIdConverterTests {
-    /**
-     * @see DATAREST-763
-     */
-    @Test
-    public void idsForUseWithinAURIShouldEncodeSpaces() {
-        assertThat(INSTANCE.toRequestId("I contain spaces", Object.class), is("I+contain+spaces"));
-    }
+	/**
+	 * @see DATAREST-763
+	 */
+	@Test
+	public void idsForUseWithinAURIShouldEncodeSpaces() {
+		assertThat(INSTANCE.toRequestId("I contain spaces", Object.class), is("I+contain+spaces"));
+	}
 
-    /**
-     * @see DATAREST-763
-     */
-    @Test
-    public void idsForUseWithinAURIShouldEncodeSpacesAtEnd() {
-        assertThat(INSTANCE.toRequestId("I contain spaces   ", Object.class), is("I+contain+spaces+++"));
-    }
+	/**
+	 * @see DATAREST-763
+	 */
+	@Test
+	public void idsForUseWithinAURIShouldEncodeSpacesAtEnd() {
+		assertThat(INSTANCE.toRequestId("I contain spaces   ", Object.class), is("I+contain+spaces+++"));
+	}
 
-    /**
-     * @see DATAREST-763
-     *
-     * This one is slightly more interesting - the %2F *may* be rejected by web servers as part of a generated URL
-     * Apache inparticular denies all URLs with %2F in the path part, for security reasons
-     * but we can't allow [raw] slashes to be present in an ID as paths could then be subverted - precisely the reason
-     * that Apache rejects the %2F.
-     *
-     * I'd propose double-encoding/decoding the '/' --> %2f --> %252f for maximum portability
-     */
-    @Test
-    public void idsForUseWithinAURIShouldEncodeToAvoidPathExploiting() {
-        assertThat(INSTANCE.toRequestId("I/contain/slashes", Object.class), is("I%252Fcontain%252Fslashes"));
-    }
+	/**
+	 * @see DATAREST-763
+	 * 
+	 * This one is slightly more interesting - the %2F *may* be rejected by web servers as part of a generated URL
+	 * Apache inparticular denies all URLs with %2F in the path part, for security reasons
+	 * but we can't allow [raw] slashes to be present in an ID as paths could then be subverted - precisely the reason
+	 * that Apache rejects the %2f.
+	 * 
+	 * I'd propose double-encoding/decoding the '/' --> %2f --> %252f for maximum portability
+	 */
+	@Test
+	public void idsForUseWithinAURIShouldEncodeToAvoidPathExploiting() {
+		assertThat(INSTANCE.toRequestId("I/contain/slashes", Object.class), is("I%252Fcontain%252Fslashes"));
+	}
 
-    /**
-     * @see DATAREST-763
-     */
-    @Test
-    public void idsForUseWithinAURIShouldEncodeQuestionMarkToAvoidQueryStringExploiting() {
-        assertThat(INSTANCE.toRequestId("IContainAQuestionMark?size=1000000", Object.class),
-                is("IContainAQuestionMark%3Fsize%3D1000000"));
-    }
+	/**
+	 * @see DATAREST-763
+	 */
+	@Test
+	public void idsForUseWithinAURIShouldEncodeQuestionMarkToAvoidQueryStringExploiting() {
+		assertThat(INSTANCE.toRequestId("IContainAQuestionMark?size=1000000", Object.class),
+				is("IContainAQuestionMark%3Fsize%3D1000000"));
+	}
 
-    /**
-     * @see DATAREST-763
-     */
-    @Test
-    public void idsForUseWithinAURIShouldDecodeQuestionMarks() {
-        assertThat((String)INSTANCE.fromRequestId("IContainAQuestionMark%3Fsize%3D1000000", Object.class),
-                is("IContainAQuestionMark?size=1000000"));
-    }
+	/**
+	 * @see DATAREST-763
+	 */
+	@Test
+	public void idsForUseWithinAURIShouldDecodeQuestionMarks() {
+		assertThat((String)INSTANCE.fromRequestId("IContainAQuestionMark%3Fsize%3D1000000", Object.class),
+				is("IContainAQuestionMark?size=1000000"));
+	}
 
-    /**
-     * @see DATAREST-763
-     */
-    @Test
-    public void idsForUseWithinAURIShouldDecodeSlashes() {
-        assertThat((String)INSTANCE.fromRequestId("I%252Fcontain%252Fslashes", Object.class),
-                is("I/contain/slashes"));
-    }
+	/**
+	 * @see DATAREST-763
+	 */
+	@Test
+	public void idsForUseWithinAURIShouldDecodeSlashes() {
+		assertThat((String)INSTANCE.fromRequestId("I%252Fcontain%252Fslashes", Object.class),
+				is("I/contain/slashes"));
+	}
 
-    /**
-     * @see DATAREST-763
-     */
-    @Test
-    public void idsForUseWithinAURIShouldDecodeSpaces() {
-        assertThat((String)INSTANCE.fromRequestId("I%20contain%20spaces", Object.class),
-                is("I contain spaces"));
-    }
+	/**
+	 * @see DATAREST-763
+	 */
+	@Test
+	public void idsForUseWithinAURIShouldDecodeSpaces() {
+		assertThat((String)INSTANCE.fromRequestId("I%20contain%20spaces", Object.class),
+				is("I contain spaces"));
+	}
 }
