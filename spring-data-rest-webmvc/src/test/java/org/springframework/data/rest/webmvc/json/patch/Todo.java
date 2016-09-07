12a870a6aef7ed37695d48b0f7718a17b35a2be7
Oliver Trosien <otrosien@epages.com>
2016-09-07 12:48:24 +0200

    DATAREST-889 - JsonPatchPatchConverter explicitly rejects unknown paths and values.
    JsonPatchPatchConverter now explicitly rejects problematic JSON Patch payloads (e.g. incorrect paths, incorrect values) throwing a PatchException.
    
    Original pull request: #227.

--- spring-data-rest-webmvc/src/test/java/org/springframework/data/rest/webmvc/json/patch/Todo.java
+++ spring-data-rest-webmvc/src/test/java/org/springframework/data/rest/webmvc/json/patch/Todo.java
@@ -16,6 +16,7 @@
 
 package org.springframework.data.rest.webmvc.json.patch;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,6 +38,7 @@ class Todo {
 	private boolean complete;
 	private TodoType type = new TodoType();
 	private List<String> items = new ArrayList<String>();
+	private BigInteger amount;
 
 	public Todo(Long id, String description, boolean complete) {
 
