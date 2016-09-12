99aff8adf930ecf7ae09f21cb3d6f53e30c73b91
Oliver Gierke <ogierke@pivotal.io>
2016-09-12 11:02:15 +0200

    DATAREST-885 - Polishing.
    Formatting. Copyright ranges.

--- spring-data-rest-webmvc/src/main/java/org/springframework/data/rest/webmvc/json/patch/PatchOperation.java
+++ spring-data-rest-webmvc/src/main/java/org/springframework/data/rest/webmvc/json/patch/PatchOperation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 the original author or authors.
+ * Copyright 2014-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -165,7 +165,9 @@ public abstract class PatchOperation {
 	 *         otherwise.
 	 */
 	protected <T> Object evaluateValueFromTarget(Object targetObject, Class<T> entityType) {
-		return value instanceof LateObjectEvaluator ? ((LateObjectEvaluator) value).evaluate(spelExpression.getValueType(targetObject)) : value;
+
+		return value instanceof LateObjectEvaluator
+				? ((LateObjectEvaluator) value).evaluate(spelExpression.getValueType(targetObject)) : value;
 	}
 
 	/**
