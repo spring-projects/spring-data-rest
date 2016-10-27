798ff6450dbda7749861ffe3f89b11bcd28c169b
Mark Angrish <mark.angrish@gmail.com>
2016-10-27 18:11:25 +1100

    DATAREST-871 - Added Missing Neo4j Web Tests.

---
+++ spring-data-rest-tests/spring-data-rest-tests-neo4j/src/main/java/org/springframework/data/rest/tests/neo4j/Address.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2013-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.rest.tests.neo4j;
+
+import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.NodeEntity;
+
+/**
+ * @author Mark Angrish
+ */
+@NodeEntity
+public class Address {
+
+    @GraphId
+    private Long id;
+
+    private String street;
+
+    private String city;
+
+    private Country country;
+
+    public Address(String street, String city, Country country) {
+
+        this.street = street;
+        this.city = city;
+        this.country = country;
+    }
+
+    public Address() {
+
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+}
---
+++ spring-data-rest-tests/spring-data-rest-tests-neo4j/src/main/java/org/springframework/data/rest/tests/neo4j/Country.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2013-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.rest.tests.neo4j;
+
+import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.NodeEntity;
+
+/**
+ * @author Mark Angrish
+ */
+@NodeEntity
+public class Country {
+
+    @GraphId
+    private Long id;
+
+    private String code;
+
+    private String name;
+
+    public Country(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public Country() {
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
