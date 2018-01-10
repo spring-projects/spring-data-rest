9b0f0c7101cf6e9b460f560901fe61ae11b69285
Oliver Gierke <ogierke@pivotal.io>
2018-01-10 12:47:24 +0100

    DATAREST-1174 - Enable constructor properties to make sure WrappedPropertiesUnitTests continue to work.
    Removed Lombok constructor and accessor generation to avoid having to deal with the changed default in Lombok 1.16.20 not generating @ConstructorProperties by default anymore.

--- spring-data-rest-webmvc/src/test/java/org/springframework/data/rest/webmvc/json/WrappedPropertiesUnitTests.java
+++ spring-data-rest-webmvc/src/test/java/org/springframework/data/rest/webmvc/json/WrappedPropertiesUnitTests.java
@@ -18,10 +18,6 @@ package org.springframework.data.rest.webmvc.json;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.Collections;
 import java.util.List;
 
@@ -32,6 +28,8 @@ import org.springframework.data.mapping.PersistentEntity;
 import org.springframework.data.mapping.PersistentProperty;
 import org.springframework.data.mapping.context.PersistentEntities;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
@@ -130,9 +128,7 @@ public class WrappedPropertiesUnitTests {
 		assertThat(wrappedProperties.hasPersistentPropertiesForField("street"), is(false));
 	}
 
-	@Data
-	@AllArgsConstructor
-	@NoArgsConstructor
+	@JsonAutoDetect(fieldVisibility = Visibility.ANY)
 	static class OneLevelNesting {
 
 		String one;
@@ -171,9 +167,7 @@ public class WrappedPropertiesUnitTests {
 	  </code>
 	 * </pre>
 	 */
-	@Data
-	@AllArgsConstructor
-	@NoArgsConstructor
+	@JsonAutoDetect(fieldVisibility = Visibility.ANY)
 	static class MultiLevelNesting {
 
 		String multi;
@@ -182,9 +176,7 @@ public class WrappedPropertiesUnitTests {
 		@JsonUnwrapped(enabled = false) OneLevelNesting nested;
 	}
 
-	@Data
-	@AllArgsConstructor
-	@NoArgsConstructor
+	@JsonAutoDetect(fieldVisibility = Visibility.ANY)
 	static class Address {
 
 		String street;
