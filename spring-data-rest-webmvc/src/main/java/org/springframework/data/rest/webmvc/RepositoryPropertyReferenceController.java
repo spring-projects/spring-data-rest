spring-projects/spring-data-rest

commit 554d6cb27b84cebedc651a5a25b10ccfb0cf8265

DATAREST-1341 - Further API adaption for Spring HATEOAS 1.0.

spring-data-rest-webmvc/src/main/java/org/springframework/data/rest/webmvc/RepositoryPropertyReferenceController.java
@@ -57,6 +57,7 @@ import org.springframework.data.rest.core.mapping.ResourceMetadata;
 import org.springframework.data.rest.webmvc.support.BackendId;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.Link;
+import org.springframework.hateoas.LinkRelation;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceSupport;
 import org.springframework.hateoas.Resources;
@@ -294,9 +295,9 @@ class RepositoryPropertyReferenceController extends AbstractRepositoryRestContro
 
 			} else if (prop.property.isMap()) {
 
-				Map<String, Object> map = AUGMENTING_METHODS.contains(requestMethod) //
-						? (Map<String, Object>) prop.propertyValue //
-						: CollectionFactory.<String, Object> createMap(propertyType, 0);
+				Map<LinkRelation, Object> map = AUGMENTING_METHODS.contains(requestMethod) //
+						? (Map<LinkRelation, Object>) prop.propertyValue //
+						: CollectionFactory.<LinkRelation, Object> createMap(propertyType, 0);
 
 				// Add to the existing collection
 				for (Link l2 : source.getLinks()) {
@@ -314,12 +315,13 @@ class RepositoryPropertyReferenceController extends AbstractRepositoryRestContro
 									"Cannot PATCH a reference to this singular property since the property type is not a List or a Map.");
 				}
 
-				if (source.getLinks().size() != 1) {
+				if (source.getLinks().hasSingleLink()) {
 					throw new IllegalArgumentException(
 							"Must send only 1 link to update a property reference that isn't a List or a Map.");
 				}
 
-				prop.accessor.setProperty(prop.property, loadPropertyValue(prop.propertyType, source.getLinks().get(0)));
+				prop.accessor.setProperty(prop.property,
+						loadPropertyValue(prop.propertyType, source.getLinks().toList().get(0)));
 			}
 
 			publisher.publishEvent(new BeforeLinkSaveEvent(prop.accessor.getBean(), prop.propertyValue));

spring-data-rest-core/src/main/java/org/springframework/data/rest/core/mapping/CollectionResourceMapping.java
@@ -15,6 +15,8 @@
  */
 package org.springframework.data.rest.core.mapping;
 
+import org.springframework.hateoas.LinkRelation;
+
 /**
  * A custom resource mapping for collection resources.
  *
@@ -27,7 +29,7 @@ public interface CollectionResourceMapping extends ResourceMapping {
 	 *
 	 * @return
 	 */
-	String getItemResourceRel();
+	LinkRelation getItemResourceRel();
 
 	/**
 	 * Returns the {@link ResourceDescription} for the item resource.

spring-data-rest-core/src/main/java/org/springframework/data/rest/core/mapping/ParameterMetadata.java
@@ -17,6 +17,7 @@ package org.springframework.data.rest.core.mapping;
 
 import org.springframework.core.MethodParameter;
 import org.springframework.data.rest.core.annotation.Description;
+import org.springframework.hateoas.LinkRelation;
 import org.springframework.util.Assert;
 
 /**
@@ -42,8 +43,8 @@ public final class ParameterMetadata {
 		Assert.hasText(name, "Parameter name must not be null or empty!");
 		Assert.hasText(baseRel, "Method rel must not be null!");
 
-		ResourceDescription fallback = TypedResourceDescription.defaultFor(baseRel.concat(".").concat(name),
-				parameter.getParameterType());
+		ResourceDescription fallback = TypedResourceDescription
+				.defaultFor(LinkRelation.of(baseRel.concat(".").concat(name)), parameter.getParameterType());
 		Description annotation = parameter.getParameterAnnotation(Description.class);
 
 		this.description = annotation == null ? fallback : new AnnotationBasedResourceDescription(annotation, fallback);

spring-data-rest-core/src/main/java/org/springframework/data/rest/core/mapping/PersistentPropertyResourceMapping.java
@@ -22,6 +22,7 @@ import org.springframework.data.rest.core.Path;
 import org.springframework.data.rest.core.annotation.Description;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.data.util.Optionals;
+import org.springframework.hateoas.LinkRelation;
 import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 
@@ -71,11 +72,11 @@ class PersistentPropertyResourceMapping implements PropertyAwareResourceMapping 
 	 * @see org.springframework.data.rest.core.mapping.ResourceMapping#getRel()
 	 */
 	@Override
-	public String getRel() {
+	public LinkRelation getRel() {
 
-		return annotation.filter(it -> StringUtils.hasText(it.rel()))//
-				.map(it -> it.rel())//
-				.orElseGet(() -> property.getName());
+		return LinkRelation.of(annotation.filter(it -> StringUtils.hasText(it.rel())) //
+				.map(it -> it.rel()) //
+				.orElseGet(() -> property.getName()));
 	}
 
 	/*

spring-data-rest-core/src/main/java/org/springframework/data/rest/core/mapping/RepositoryAwareResourceMetadata.java
@@ -21,6 +21,7 @@ import org.springframework.data.mapping.PersistentEntity;
 import org.springframework.data.mapping.PersistentProperty;
 import org.springframework.data.repository.core.RepositoryMetadata;
 import org.springframework.data.rest.core.Path;
+import org.springframework.hateoas.LinkRelation;
 import org.springframework.util.Assert;
 
 /**
@@ -129,7 +130,7 @@ class RepositoryAwareResourceMetadata implements ResourceMetadata {
 	 * @see org.springframework.data.rest.core.mapping.CollectionResourceMapping#getCollectionRel()
 	 */
 	@Override
-	public String getRel() {
+	public LinkRelation getRel() {
 		return mapping.getRel();
 	}
 
@@ -138,7 +139,7 @@ class RepositoryAwareResourceMetadata implements ResourceMetadata {
 	 * @see org.springframework.data.rest.core.mapping.CollectionResourceMapping#getSingleResourceRel()
 	 */
 	@Override
-	public String getItemResourceRel() {
+	public LinkRelation getItemResourceRel() {
 		return mapping.getItemResourceRel();
 	}
 

spring-data-rest-core/src/main/java/org/springframework/data/rest/core/mapping/RepositoryCollectionResourceMapping.java
@@ -22,6 +22,7 @@ import org.springframework.data.repository.core.RepositoryMetadata;
 import org.springframework.data.rest.core.Path;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.hateoas.LinkRelation;
 import org.springframework.hateoas.RelProvider;
 import org.springframework.hateoas.core.EvoInflectorRelProvider;
 import org.springframework.util.Assert;
@@ -111,18 +112,18 @@ class RepositoryCollectionResourceMapping implements CollectionResourceMapping {
 	 * @see org.springframework.data.rest.core.mapping.ResourceMapping#getRel()
 	 */
 	@Override
-	public String getRel() {
+	public LinkRelation getRel() {
 
-		String fallback = domainTypeMapping.getRel();
+		LinkRelation fallback = domainTypeMapping.getRel();
 
 		if (repositoryAnnotation != null) {
 			String rel = repositoryAnnotation.collectionResourceRel();
-			return StringUtils.hasText(rel) ? rel : fallback;
+			return StringUtils.hasText(rel) ? LinkRelation.of(rel) : fallback;
 		}
 
 		if (annotation != null) {
 			String rel = annotation.rel();
-			return StringUtils.hasText(rel) ? rel : fallback;
+			return StringUtils.hasText(rel) ? LinkRelation.of(rel) : fallback;
 		}
 
 		return fallback;
@@ -133,13 +134,13 @@ class RepositoryCollectionResourceMapping implements CollectionResourceMapping {
 	 * @see org.springframework.data.rest.core.mapping.CollectionResourceMapping#getSingleResourceRel()
 	 */
 	@Override
-	public String getItemResourceRel() {
+	public LinkRelation getItemResourceRel() {
 
-		String fallback = domainTypeMapping.getItemResourceRel();
+		LinkRelation fallback = domainTypeMapping.getItemResourceRel();
 
 		if (repositoryAnnotation != null) {
 			String rel = repositoryAnnotation.itemResourceRel();
-			return StringUtils.hasText(rel) ? rel : fallback;
+			return StringUtils.hasText(rel) ? LinkRelation.of(rel) : fallback;
 		}
 
 		return fallback;

spring-data-rest-core/src/main/java/org/springframework/data/rest/core/mapping/RepositoryMethodResourceMapping.java
@@ -30,6 +30,7 @@ import org.springframework.data.repository.core.RepositoryMetadata;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.Path;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.hateoas.LinkRelation;
 import org.springframework.hateoas.core.AnnotationAttribute;
 import org.springframework.hateoas.core.MethodParameters;
 import org.springframework.util.Assert;
@@ -46,7 +47,7 @@ class RepositoryMethodResourceMapping implements MethodResourceMapping {
 	private static final AnnotationAttribute PARAM_VALUE = new AnnotationAttribute(Param.class);
 
 	private final boolean isExported;
-	private final String rel;
+	private final LinkRelation rel;
 	private final Path path;
 	private final Method method;
 	private final boolean paging;
@@ -70,14 +71,15 @@ class RepositoryMethodResourceMapping implements MethodResourceMapping {
 		Assert.notNull(resourceMapping, "ResourceMapping must not be null!");
 
 		RestResource annotation = AnnotationUtils.findAnnotation(method, RestResource.class);
-		String resourceRel = resourceMapping.getRel();
+		LinkRelation resourceRel = resourceMapping.getRel();
 
 		this.isExported = annotation != null ? annotation.exported() : exposeMethodsByDefault;
-		this.rel = annotation == null || !StringUtils.hasText(annotation.rel()) ? method.getName() : annotation.rel();
+		this.rel = LinkRelation
+				.of(annotation == null || !StringUtils.hasText(annotation.rel()) ? method.getName() : annotation.rel());
 		this.path = annotation == null || !StringUtils.hasText(annotation.path()) ? new Path(method.getName())
 				: new Path(annotation.path());
 		this.method = method;
-		this.parameterMetadata = discoverParameterMetadata(method, resourceRel.concat(".").concat(rel));
+		this.parameterMetadata = discoverParameterMetadata(method, resourceRel.value().concat(".").concat(rel.value()));
 
 		List<Class<?>> parameterTypes = Arrays.asList(method.getParameterTypes());
 
@@ -114,7 +116,7 @@ class RepositoryMethodResourceMapping implements MethodResourceMapping {
 	 * @see org.springframework.data.rest.core.mapping.ResourceMapping#getRel()
 	 */
 	@Override
-	public String getRel() {
+	public LinkRelation getRel() {
 		return rel;
 	}
 

spring-data-rest-core/src/main/java/org/springframework/data/rest/core/mapping/ResourceMapping.java
@@ -16,6 +16,7 @@
 package org.springframework.data.rest.core.mapping;
 
 import org.springframework.data.rest.core.Path;
+import org.springframework.hateoas.LinkRelation;
 
 /**
  * Mapping information for components to be exported as REST resources.
@@ -36,7 +37,7 @@ public interface ResourceMapping {
 	 *
 	 * @return will never be {@literal null}.
 	 */
-	String getRel();
+	LinkRelation getRel();
 
 	/**
 	 * Returns the path the resource is exposed under.

commit 45fb781d33da121bf155f15f1a3b2753688d0483

Fix for a bug around single link PUTs introduced in 554d6cb2

spring-data-rest-webmvc/src/main/java/org/springframework/data/rest/webmvc/RepositoryPropertyReferenceController.java
@@ -318,7 +318,7 @@ class RepositoryPropertyReferenceController extends AbstractRepositoryRestContro
 									"Cannot PATCH a reference to this singular property since the property type is not a List or a Map.");
 				}
 
-				if (source.getLinks().hasSingleLink()) {
+				if (!source.getLinks().hasSingleLink()) {
 					throw new IllegalArgumentException(
 							"Must send only 1 link to update a property reference that isn't a List or a Map.");
 				}