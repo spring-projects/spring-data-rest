c11ab6cb275153f978b814a3d25a2ad700b3daf6
Mark Paluch <mpaluch@pivotal.io>
2019-06-21 11:04:53 +0200

    DATAREST-1397 - Adapt to CORS changes in AbstractHandlerMapping.
    We now enable CORS handling for all requests by overriding the newly introduced hasCorsConfigurationSource method. We cannot detect CORS configuration handling solely on the handler but require path headers to resolve repository interface mappings.

--- spring-data-rest-webmvc/src/main/java/org/springframework/data/rest/webmvc/RepositoryRestHandlerMapping.java
+++ spring-data-rest-webmvc/src/main/java/org/springframework/data/rest/webmvc/RepositoryRestHandlerMapping.java
@@ -192,6 +192,15 @@ public class RepositoryRestHandlerMapping extends BasePathAwareHandlerMapping {
 
 	/*
 	 * (non-Javadoc)
+	 * @see org.springframework.web.servlet.handler.AbstractHandlerMapping#hasCorsConfigurationSource(java.lang.Object)
+	 */
+	@Override
+	protected boolean hasCorsConfigurationSource(Object handler) {
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
 	 * @see org.springframework.web.servlet.handler.AbstractHandlerMapping#extendInterceptors(java.util.List)
 	 */
 	@Override
