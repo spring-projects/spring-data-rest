spring-projects/spring-data-rest

commit 4b36f794dca3866cd817f7f48f05d4ab6b8edd34

Avoid RepositoryRestConfigurerDelegate exposed as bean.
Improving the configuration situation for RRCD, whose bean declaration required access to all RepositoryRestConfigurer instances while implementing the interface itself, too.  We now rather hide the creation of that instance in a Lazy as it doesn't need any kind of lifecycle management.

spring-data-rest-webmvc/src/main/java/org/springframework/data/rest/webmvc/config/RepositoryRestMvcConfiguration.java
@@ -259,15 +259,14 @@ public class RepositoryRestMvcConfiguration extends HateoasAwareSpringDataWebCon
 				new DefaultRepositoryInvokerFactory(repositories.get(), defaultConversionService), getEntityLookups()));
 
 		this.defaultConversionService = new DefaultFormattingConversionService();
-		this.configurerDelegate = Lazy.of(() -> context.getBean(RepositoryRestConfigurerDelegate.class));
+
+		this.configurerDelegate = Lazy.of(() -> {
+			return new RepositoryRestConfigurerDelegate(context.getBeansOfType(RepositoryRestConfigurer.class).values());
+		});
+
 		this.repositoryRestConfiguration = Lazy.of(() -> context.getBean(RepositoryRestConfiguration.class));
 		this.pageableResolver = Lazy.of(() -> context.getBean(HateoasPageableHandlerMethodArgumentResolver.class));
 		this.sortResolver = Lazy.of(() -> context.getBean(HateoasSortHandlerMethodArgumentResolver.class));
-	}
-
-	@Bean
-	public static RepositoryRestConfigurerDelegate configurerDelegate(List<RepositoryRestConfigurer> configurers) {
-		return new RepositoryRestConfigurerDelegate(configurers);
 	}
 
 	/*
@@ -305,8 +304,7 @@ public class RepositoryRestMvcConfiguration extends HateoasAwareSpringDataWebCon
 	@Bean
 	@Qualifier
 	public DefaultFormattingConversionService defaultConversionService(PersistentEntities persistentEntities,
-			RepositoryInvokerFactory repositoryInvokerFactory, Repositories repositories,
-			RepositoryRestConfigurerDelegate configurerDelegate) {
+			RepositoryInvokerFactory repositoryInvokerFactory, Repositories repositories) {
 
 		DefaultFormattingConversionService conversionService = (DefaultFormattingConversionService) defaultConversionService;
 
@@ -316,7 +314,7 @@ public class RepositoryRestMvcConfiguration extends HateoasAwareSpringDataWebCon
 		conversionService.addConverter(StringToLdapNameConverter.INSTANCE);
 		addFormatters(conversionService);
 
-		configurerDelegate.configureConversionService(conversionService);
+		configurerDelegate.get().configureConversionService(conversionService);
 
 		return conversionService;
 	}
@@ -326,11 +324,11 @@ public class RepositoryRestMvcConfiguration extends HateoasAwareSpringDataWebCon
 	 * {@link org.springframework.validation.Validator} instances assigned to specific domain types.
 	 */
 	@Bean
-	public ValidatingRepositoryEventListener validatingRepositoryEventListener(ObjectFactory<PersistentEntities> entities,
-			RepositoryRestConfigurerDelegate configurerDelegate) {
+	public ValidatingRepositoryEventListener validatingRepositoryEventListener(
+			ObjectFactory<PersistentEntities> entities) {
 
 		ValidatingRepositoryEventListener listener = new ValidatingRepositoryEventListener(entities);
-		configurerDelegate.configureValidatingRepositoryEventListener(listener);
+		configurerDelegate.get().configureValidatingRepositoryEventListener(listener);
 
 		return listener;
 	}
@@ -775,7 +773,7 @@ public class RepositoryRestMvcConfiguration extends HateoasAwareSpringDataWebCon
 			@Qualifier("halJacksonHttpMessageConverter") TypeConstrainedMappingJackson2HttpMessageConverter halJacksonHttpMessageConverter,
 			@Qualifier("halFormsJacksonHttpMessageConverter") TypeConstrainedMappingJackson2HttpMessageConverter halFormsJacksonHttpMessageConverter,
 			AlpsJsonHttpMessageConverter alpsJsonHttpMessageConverter,
-			UriListHttpMessageConverter uriListHttpMessageConverter, RepositoryRestConfigurerDelegate configurerDelegate,
+			UriListHttpMessageConverter uriListHttpMessageConverter,
 			RepositoryRestConfiguration repositoryRestConfiguration) {
 
 		List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
@@ -800,7 +798,7 @@ public class RepositoryRestMvcConfiguration extends HateoasAwareSpringDataWebCon
 		messageConverters.add(fallbackJsonConverter);
 		messageConverters.add(uriListHttpMessageConverter);
 
-		configurerDelegate.configureHttpMessageConverters(messageConverters);
+		configurerDelegate.get().configureHttpMessageConverters(messageConverters);
 
 		return messageConverters;
 	}

spring-data-rest-webmvc/src/main/java/org/springframework/data/rest/webmvc/config/RepositoryRestConfigurerDelegate.java
@@ -34,7 +34,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  * @author Oliver Gierke
  * @soundtrack Florian Reichelt & Max Ender - Abschlusskonzert (https://www.youtube.com/watch?v=5WP0P-ndinY)
  */
-public class RepositoryRestConfigurerDelegate implements RepositoryRestConfigurer {
+class RepositoryRestConfigurerDelegate implements RepositoryRestConfigurer {
 
 	private final Iterable<RepositoryRestConfigurer> delegates;
 

commit 6c007487c7aa2fad0cb03109a959f997eca41d41

RepositoryRestConfigurer are again used in declared order.
4b36f79 introduced a regression by consuming registered RepositoryRestConfigurer instances via ApplicationContext.getBeansOfType(…).values() which, unlike the previous consumption via a List<RRC> in an @Bean method, is losing the declared order of the RRC instances as ….getBeansOfType() is a Map.

We now rather use an ObjectProvider and its ….orderedStream() method to consume the registered instances ordered properly.

Fixes #1995.

spring-data-rest-webmvc/src/main/java/org/springframework/data/rest/webmvc/config/RepositoryRestMvcConfiguration.java
@@ -23,6 +23,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.BeanClassLoaderAware;
 import org.springframework.beans.factory.BeanFactoryUtils;
@@ -259,7 +260,10 @@ public class RepositoryRestMvcConfiguration extends HateoasAwareSpringDataWebCon
 		this.defaultConversionService = new DefaultFormattingConversionService();
 
 		this.configurerDelegate = Lazy.of(() -> {
-			return new RepositoryRestConfigurerDelegate(context.getBeansOfType(RepositoryRestConfigurer.class).values());
+
+			return new RepositoryRestConfigurerDelegate(context.getBeanProvider(RepositoryRestConfigurer.class)
+					.orderedStream()
+					.collect(Collectors.toList()));
 		});
 
 		this.repositoryRestConfiguration = Lazy.of(() -> context.getBean(RepositoryRestConfiguration.class));

spring-data-rest-webmvc/src/test/java/org/springframework/data/rest/webmvc/config/RepositoryRestMvConfigurationIntegrationTests.java
@@ -28,7 +28,6 @@ import javax.naming.ldap.LdapName;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -36,6 +35,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.core.annotation.Order;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
@@ -45,6 +45,8 @@ import org.springframework.data.rest.webmvc.RepositoryLinksResource;
 import org.springframework.data.rest.webmvc.RestMediaTypes;
 import org.springframework.data.rest.webmvc.alps.AlpsJsonHttpMessageConverter;
 import org.springframework.data.rest.webmvc.json.PersistentEntityJackson2Module;
+import org.springframework.data.util.Lazy;
+import org.springframework.data.util.Streamable;
 import org.springframework.data.web.HateoasPageableHandlerMethodArgumentResolver;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.hateoas.MediaTypes;
@@ -54,6 +56,7 @@ import org.springframework.hateoas.server.mvc.TypeConstrainedMappingJackson2Http
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -192,6 +195,22 @@ public class RepositoryRestMvConfigurationIntegrationTests {
 		assertThat(service.canConvert(String.class, LdapName.class)).isTrue();
 	}
 
+	@Test // #1995
+	@SuppressWarnings("unchecked")
+	public void registersRepositoryRestConfigurersInDeclaredOrder() {
+
+		RepositoryRestMvcConfiguration configuration = context.getBean(RepositoryRestMvcConfiguration.class);
+		ExtendingConfiguration userConfig = context.getBean(ExtendingConfiguration.class);
+
+		Lazy<RepositoryRestConfigurerDelegate> delegate = (Lazy<RepositoryRestConfigurerDelegate>) ReflectionTestUtils
+				.getField(configuration, "configurerDelegate");
+		Iterable<RepositoryRestConfigurer> configurations = (Iterable<RepositoryRestConfigurer>) ReflectionTestUtils
+				.getField(delegate.get(), "delegates");
+
+		assertThat(Streamable.of(configurations).toList())
+				.containsSequence(userConfig.otherConfigurer(), userConfig.configurer());
+	}
+
 	private static ObjectMapper getObjectMapper() {
 
 		AbstractJackson2HttpMessageConverter converter = context.getBean("halJacksonHttpMessageConverter",
@@ -214,6 +233,7 @@ public class RepositoryRestMvConfigurationIntegrationTests {
 		}
 
 		@Bean
+		@Order(200)
 		RepositoryRestConfigurer configurer() {
 
 			return RepositoryRestConfigurer.withConfig(config -> {
@@ -224,6 +244,12 @@ public class RepositoryRestMvConfigurationIntegrationTests {
 				config.setLimitParamName("mySize");
 				config.setSortParamName("mySort");
 			});
+		}
+
+		@Bean
+		@Order(100)
+		RepositoryRestConfigurer otherConfigurer() {
+			return RepositoryRestConfigurer.withConfig(__ -> {});
 		}
 	}
 