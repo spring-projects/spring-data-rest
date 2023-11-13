e60bdac72e5c854000d9381b0aed015a41dd2dd0
mikoto2000 <mikoto2000@gmail.com>
2023-11-13 20:17:19 +0000

    DATAREST-1280 Add support sort collection resource by associated property.
    Closes #1641
    Related tickets #1386 #1343

--- spring-data-rest-webmvc/src/main/java/org/springframework/data/rest/webmvc/json/JacksonMappingAwareSortTranslator.java
+++ spring-data-rest-webmvc/src/main/java/org/springframework/data/rest/webmvc/json/JacksonMappingAwareSortTranslator.java
@@ -192,10 +192,6 @@ public class JacksonMappingAwareSortTranslator {
 
 				for (PersistentProperty<?> persistentProperty : persistentProperties) {
 
-					if (associations.isLinkableAssociation(persistentProperty)) {
-						return Collections.emptyList();
-					}
-
 					persistentPropertyPath.add(persistentProperty.getName());
 				}
 
