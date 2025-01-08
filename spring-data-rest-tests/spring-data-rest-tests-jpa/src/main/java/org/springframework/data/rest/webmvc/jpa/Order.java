feabd2e48913878f1105e31b9383b4ac30b56ad2
Mark Paluch <mark.paluch@broadcom.com>
2025-01-08 09:49:50 +0100

    Upgrade to Hibernate 7.0.0.Beta1.
    Closes #2444

--- spring-data-rest-tests/spring-data-rest-tests-jpa/src/main/java/org/springframework/data/rest/webmvc/jpa/Order.java
+++ spring-data-rest-tests/spring-data-rest-tests-jpa/src/main/java/org/springframework/data/rest/webmvc/jpa/Order.java
@@ -18,7 +18,6 @@ package org.springframework.data.rest.webmvc.jpa;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
@@ -30,13 +29,14 @@ import java.util.List;
 
 /**
  * @author Oliver Gierke
+ * @author Mark Paluch
  */
 @Entity
 @Table(name = "ORDERS")
 public class Order {
 
-	@Id @GeneratedValue //
-	private Long id;
+	@Id
+	@SequenceGenerator private Long id;
 	@ManyToOne(fetch = FetchType.LAZY) //
 	private Person creator;
 	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true) //
